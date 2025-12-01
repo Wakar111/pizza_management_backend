f1075ad0b5cf9735789f9a6e1c4218d848ee32e7
Wakar <waqar786@hotmail.de>
2025-12-01 20:14:47 +0100

    change to secure gmail port

--- server/server.js
+++ server/server.js
@@ -19,14 +19,18 @@ app.use(cors({
 app.use(express.json())
 
 // Create email transporter
+// Create email transporter
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
   port: process.env.EMAIL_PORT,
-  secure: false, // true for 465, false for other ports
+  secure: process.env.EMAIL_PORT == 465, // true for 465, false for other ports
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD
-  }
+  },
+  connectionTimeout: 10000, // 10 seconds
+  greetingTimeout: 10000,
+  socketTimeout: 10000
 })
 
 // Verify transporter configuration
