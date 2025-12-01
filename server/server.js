ffc4728c798b4ed1cba8d63a81ed9a0bb2104690
Wakar <waqar786@hotmail.de>
2025-12-01 20:34:03 +0100

    make secure false

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ app.use(express.json())
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
   port: process.env.EMAIL_PORT,
-  secure: process.env.EMAIL_PORT == 465, // true for 465, false for other ports
+  secure: false, // true for 465, false for other ports
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD
